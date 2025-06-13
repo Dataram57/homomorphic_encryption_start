0c5cf6226ea21a7a9ff63800c62534ad826dd9dc
Dataram57 <dataram66657@gmail.com>
2025-06-13 15:50:49 +0200

    noise budget and more complex math

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const SEAL = require('node-seal/allows_wasm_node_umd');
     // Set up encryption parameters
     const schemeType = seal.SchemeType.bfv;
     const securityLevel = seal.SecurityLevel.tc128;
-    const polyModulusDegree = 4096;
-    const bitSizes = [36, 36, 37];
+    const polyModulusDegree = 8192;
+    const bitSizes = [36, 36, 36, 36, 37];
     const bitSize = 20;
 
     const encParms = seal.EncryptionParameters(schemeType);
@@ -32,7 +32,7 @@ const SEAL = require('node-seal/allows_wasm_node_umd');
     const keyGenerator = seal.KeyGenerator(context);
     const publicKey = keyGenerator.createPublicKey();
     const secretKey = keyGenerator.secretKey();
-    const relinKeys = keyGenerator.createRelinKeys();  // Needed for multiplication
+    const relinKeys = keyGenerator.createRelinKeys(7);  // Needed for multiplication
 
     // Set up encryptor, evaluator, decryptor
     const encryptor = seal.Encryptor(context, publicKey);
@@ -70,14 +70,27 @@ const SEAL = require('node-seal/allows_wasm_node_umd');
         
         // Compute (x + 1) * x
         let result
-        result = evaluator.addPlain(ciphertext, batchEncoder.encode(Int32Array.from([1])));
+        checkNoise("r = x", ciphertext, decryptor);
+        result = evaluator.addPlain(ciphertext, batchEncoder.encode(Int32Array.from([1]))); //x+1
+        checkNoise("r = r + 1", result, decryptor);
+        result = evaluator.square(result); //x*x
+        checkNoise("r = r ** 2", result, decryptor);
+        result = evaluator.relinearize(result, relinKeys); // Reduce size
+        checkNoise("r = r", result, decryptor);
+        result = evaluator.multiplyPlain(result, batchEncoder.encode(Int32Array.from([3]))); //x*x
+        checkNoise("r = r * 3", result, decryptor);
         result = evaluator.multiply(result, ciphertext);  // (x + x) * x
-        evaluator.relinearize(result, relinKeys);  // Critical: reduce ciphertext size
-
+        checkNoise("r = r * x", result, decryptor);
+        result = evaluator.relinearize(result, relinKeys);  // Critical: reduce ciphertext size
+        checkNoise("r = r", result, decryptor);
         //return
         return result;
     }
     
+    function checkNoise(msg, ct, decryptor) {
+        console.log(msg + ",", "Noise budget:", decryptor.invariantNoiseBudget(ct));
+    }
+
     // Wrapper function
     async function Compute(x) {
         // Encode the value (using BatchEncoder)
@@ -109,7 +122,7 @@ const SEAL = require('node-seal/allows_wasm_node_umd');
     const x = 5;
     const result = await Compute(x);
     console.log('x = ' + x);
-    console.log('(x + 1) * x =', result);
+    console.log('3 * ((x + 1) ** 2) * x =', result);
 
     // Clean up
     [encParms, context, keyGenerator, publicKey, secretKey, relinKeys, 
